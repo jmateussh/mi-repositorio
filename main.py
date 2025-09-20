9289cdf463c7d0862d6274ca8f39434947610e21
jmateussh <shadowfenixe@gmail.com>
2025-09-20 11:02:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL" : "una respuesta a una broma",
+            "SHEESH" : "ligera desaprobación",
+            "CREEPY" : "aterrador, siniestro",
+            "AGGRO" : "ponerse agresivo/enojado"
+            }
+count = 0
+while True:
+    count +=1
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("esa palabra no existe en el diccionario")
+    if count == 5:
+        break
